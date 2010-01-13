4a883dcc84a24d0b0dda2a3c9fb4d5f58e47d9fa
Anna Billstrom <banane@gmail.com>
2010-01-12 16:49:23 -0800

    cleanup layout header refs

--- lib/carousel_tag.rb
+++ lib/carousel_tag.rb
@@ -6,13 +6,16 @@ module CarouselTag
 
     %{ <div id="carousel">
         	<ul>
-        		<li><img src="images/carousel/#{tag.attr['picture_1']}" width="500" height="213"  /><p>#{tag.attr['caption_1']}</p></li>
-        		<li><img src="images/carousel/#{tag.attr['picture_2']}"   width="500" height="213 /><p>#{tag.attr['caption_2']}</p></li>
-        				<li><img src="images/carousel/#{tag.attr['picture_3']}" width="500" height="213"  /><p>#{tag.attr['caption_3']}</p></li>
+        		<li><img src="images/carousel/#{tag.attr['picture_1']}" /><p>#{tag.attr['caption_1']}</p></li>
+        		<li><img src="images/carousel/#{tag.attr['picture_2']}"   /><p>#{tag.attr['caption_2']}</p></li>
+        				<li><img src="images/carousel/#{tag.attr['picture_3']}"/><p>#{tag.attr['caption_3']}</p></li>
         	</ul>
         </div>
       #{tag.expand}
     }
   end
-      
+  tag "carousel_batch" do |tag|
+  
+  end
+    
 end
---
+++ public/javascript/carousel.js
@@ -0,0 +1,6 @@
+$(function(){
+	$('#carousel').infiniteCarousel({
+		displayTime: 6000,
+		textholderHeight : .25
+	});
+});
