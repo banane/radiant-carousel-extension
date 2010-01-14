1aab5c0eac75a0e3d84f3155824048a2823ab447
Anna Billstrom <banane@gmail.com>
2010-01-13 18:38:37 -0800

    Added custom tags

--- carousel_extension.rb
+++ carousel_extension.rb
@@ -11,23 +11,6 @@ class CarouselExtension < Radiant::Extension
   def activate
     Page.send :include, CarouselTag
     
-
-    ApplicationController.class_eval do
-      before_filter :load_carousel_array
-      
-      # create and load image array from public directory
-      def load_carousel_array
-        @carousel_imgs = []
-        dirpath = RAILS_ROOT + '/public/images/carousel/'
-        print "************************#{dirpath}"
-        Dir.foreach(dirpath) do |entry|
-          filepath = dirpath + entry
-          if(File.file?(filepath))
-            @carousel_imgs.push(filepath)        
-          end
-        end
-      end    
-    end
   end
   
   def deactivate
--- lib/carousel_tag.rb
+++ lib/carousel_tag.rb
@@ -1,36 +1,21 @@
 module CarouselTag
   include Radiant::Taggable
   
-  desc "Creates a rotating image banner within a page"
-  tag "carousel" do |tag|
-
-    %{ <div id="headerCarousel">
-        	<ul>
-        		<li><img src="images/carousel/#{tag.attr['picture_1']}" /><p>#{tag.attr['caption_1']}</p></li>
-        		<li><img src="images/carousel/#{tag.attr['picture_2']}"   /><p>#{tag.attr['caption_2']}</p></li>
-        				<li><img src="images/carousel/#{tag.attr['picture_3']}"/><p>#{tag.attr['caption_3']}</p></li>
-        	</ul>
-        </div>
-      #{tag.expand}
-    }
+  desc "Contains the panels of the carousel, and other objects.
+  tag 'carousel' do |tag|
+    content = ' <div id="headerCarousel"><ul> '
+    content << tag.expand
+    content << "</ul></div>"
   end
   
-  desc "Loads images from a common directory and displays with no mark-up"
-  tag "carousel_batch" do |tag|
-    html = '<div id="headerCarousel"><ul>'
+  desc "Prints out the individual panels of the carousel."
+  tag 'carousel_image' do |tag|
+    %{
+      <li><a href="#{tag.attr['link']}"><img src="#{tag.attr['src']}"></a>
+      <p>#{tag.attr['caption']} </p>    
+    }
+  end  
     
-    @carousel_imgs = []
-    dirpath = RAILS_ROOT + '/public/images/carousel/'
-    Dir.foreach(dirpath) do |entry|
-      filepath = dirpath + entry
-      imgpath = '/images/carousel/' + entry
-      if(File.file?(filepath))
-        html += "<li><img src=" + imgpath + ' target="_top"></li>'      
-      end
-    end
+  
 
-    
-    html += '</ul></div>'
-    html << tag.expand
-  end    
 end
